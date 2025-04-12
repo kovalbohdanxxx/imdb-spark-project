58b915e2fb23b17b4ed4842930bab25e68cb7081
kovalbohdanxxx <kovalbohdanxxx@gmail.com>
2025-04-12 19:56:35 +0300

    Changed spark session name + added call get_movies_available_in_ukrainian function in main

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 from pyspark.sql import SparkSession # type: ignore
 
+from business_questions import *
 from data_loader import *
+from utils import *
 
-spark = SparkSession.builder.appName("DataLoading").getOrCreate()
+spark = SparkSession.builder.appName("IMDbDataAnalysis").getOrCreate()
 
-df = load_title_ratings(spark)
+# Get all movies that are available in the Ukrainian language.
+ukrainian_movies = get_movies_available_in_ukrainian(load_title_akas(spark))
+ukrainian_movies.show(10, truncate=False)
 
-print(df.count())
-print(df.columns)
 
-df.show(50)
-df.printSchema()
+# sudo docker run -v <path_to_folder_with_dataset>:/data imdb-spark-img
+# sudo docker run imdb-spark-img
